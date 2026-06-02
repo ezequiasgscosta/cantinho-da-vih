5775667a38666f3a6a639ddbc23d9987b620a09f
ezequias costa <bieldotor@gmail.com>
2026-06-02 01:36:26 -0300

    pronto

--- src/components/Cabecario.jsx
+++ src/components/Cabecario.jsx
@@ -5,8 +5,8 @@ function Cabecario() {
 bg-gradient-to-r from-pink-400 to-purple-500 flex items-center justify-around width-full p-4
 ">
             <div><img className="w-14" src={logo} alt="Logo do Cantinho da Vih" width="200" /></div>
-            <div>
-                <h1 className="text-3xl font-bold text-white">Cantinho Da Vih</h1>
+            <div className="lg:w-1/2">
+                <h1 className="text-3xl  font-bold text-white">Cantinho Da Vih</h1>
             </div>
             
         </div>
