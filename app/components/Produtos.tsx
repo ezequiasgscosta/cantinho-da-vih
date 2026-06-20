c899efc1f5c010a53e5f97f2eadc48a47b8d0da6
Ezequias Gabriel <bieldotor@gmail.com>
2026-06-20 01:22:11 -0300

    nome, descrição,preço. tudo vindo dinamicamente do banco de dados

--- app/components/Produtos.tsx
+++ app/components/Produtos.tsx
@@ -4,9 +4,8 @@ import ImageIlustrativa from "../../public/window.svg"
 import { supabase } from "../../lib/supabase"
 import { useEffect, useState } from "react"
 
-
 export default function Produtos() {
-    const [bolos, setBolos] = useState([]);
+    const [bolos, setBolos] = useState<bolo[]>([]);
     useEffect(() => {
         async function buscarproduto() {
             const { data, error } = await supabase.from("bolo").select("*")
@@ -23,27 +22,54 @@ export default function Produtos() {
     },
         []
     )
-    return (
 
-        interface bolo{
+    //falta criar a tabela aqui 
+    //e puxar do banco de dados
+
+    interface categorias {
+        id:number
+        nome:string
+    }
+
+      interface bolo{
             id:number
-            nome:text
+            nome:string
             preco:number
+            descricao:string
         }
 
-        <div className="border w-full h-screen bg-blue-400 ">
+    return (
+
+      
+
+        <div className="border w-full h-screen  ">
+            <h1 className="">Bolos</h1>
             {bolos.map((bolo) => (
 
                 <div key={bolo.id}
-                    className="border w-1/5 h-1/3 text-center">
+                    className="border border-black/10 h-32 items-center  grid
+                                grid-cols-2
+                                sm:grid-cols-2
+                                md:grid-cols-3
+                                lg:grid-cols-4
+                                xl:grid-cols-5
+                                gap-4 text-center
+                                ">
+                                    
+
+                    <div className="ml-6">
+                    <h2 className="text-bold text-xl text-center">{bolo.nome}</h2>
+                       <h2>{bolo.descricao}</h2>
+                    <h1 className="text-lg font-medium">Preço ${bolo.preco}</h1> 
+                    </div>
+                    
+
                     <Image className="m-auto"
                         src={ImageIlustrativa}
                         alt="bolo de pote"
-                        width={50}
-                        height={50}
+                        width={100}
+                        height={90}
                     />
-                    <h2>{bolo.nome}</h2>
-                    <h1>Preço ${bolo.preco}</h1>
                 </div>
             ))
             }
